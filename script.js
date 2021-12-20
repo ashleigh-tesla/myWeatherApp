1c360e6b263cb459c710ccf038c1535a86ea7f9e
ashleigh-tesla <84648968+ashleigh-tesla@users.noreply.github.com>
2021-12-20 15:38:04 +0200

    created index.html ; README.md ; script.js ; style.css & settings.json

---
+++ script.js
@@ -0,0 +1,41 @@
+let weather = {
+    apiKey: "f50e23a85aa1a369329fa12c9752f1e0",
+    fetchWeather: function(city) {
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' +
+                city +
+                '&units=metric&appid=' +
+                this.apiKey)
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed)
+        document.querySelector(".city").innerText = "Weather in " + name
+        document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/' + icon + '@2x.png'
+        document.querySelector('.description').innerText = description
+        document.querySelector('.temp').innerText = temp + '°C'
+        document.querySelector('.humidity').innerText = "Humidity : " + humidity + '%'
+        document.querySelector('.wind').innerText = "Wind Speed : " + speed + "km/h"
+        document.querySelector('.weather').classList.remove('loading')
+        document.body.style.backgroundImage = 'url("https://source.unsplash.com/random/' + name + '")'
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value)
+    }
+}
+
+document.querySelector('.search button').addEventListener("click", function() {
+    weather.search()
+})
+document.querySelector('.search-bar').addEventListener("keyup", function(event) {
+    if (event.key == 'Enter') {
+        weather.search()
+    } else {
+        return
+    }
+})
+weather.fetchWeather('Rusape')
